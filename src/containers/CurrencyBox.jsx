79ff740a2974f9784114f468afd6310e6e38b1c4
Gaurav Saxena <gvnsaxena@gmail.com>
2019-01-06 21:35:58 +0000

    removed balance components

--- src/containers/CurrencyBox.jsx
+++ src/containers/CurrencyBox.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CurrencyDropdown from '../components/CurrencyDropdown.jsx';
-import Balance from '../components/Balance.jsx';
 import RateBox from '../components/RateBox.jsx';
 class CurrencyBox extends Component {
   render() {
@@ -9,7 +8,6 @@ class CurrencyBox extends Component {
         <CurrencyDropdown dispatch={this.props.dispatch} 
           disableSelectBox={this.props.disableSelectBox} 
           currency={this.props.currency.rates} selectId={this.props.selectId}/>
-        <Balance/>
         <RateBox exSymbolValue={this.props.exSymbolValue} inputBoxId={this.props.inputBoxId} 
           disableInputBox={this.props.disableInputBox} 
           selectId={this.props.selectId}/>
